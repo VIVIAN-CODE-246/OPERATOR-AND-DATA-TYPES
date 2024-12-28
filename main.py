188baf779061656aff8b2eb085bdb444323e45ab
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2024-12-28 13:49:06 +0200

    OPERATORS AND DATA TYPES

---
+++ main.py
@@ -0,0 +1,25 @@
+name="100"
+age = 14
+weight = 48.9
+is_student = True
+
+print(type(is_student))
+
+age = bool(age)
+print(age)
+
+name = float(name)
+print(name)
+
+num1 = 100
+num2 = 225
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+print(num2 ** 2)
+print(num1 ** 0.5)
+print(num1 == num2)
+print(num1 > num2)
+print(num2 < num1)
+print(num1 != num2)
